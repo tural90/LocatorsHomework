90a54253d895843f1510a98e64a78117199dd789
turalfataliyev <55106827+turalfataliyev@users.noreply.github.com>
2019-12-29 01:02:33 -0500

    Update Ebay.java

--- src/test/java/com/cybertek/tests/Ebay.java
+++ src/test/java/com/cybertek/tests/Ebay.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//test
 public class Ebay {
     public static void main(String[] args) throws InterruptedException{
 
